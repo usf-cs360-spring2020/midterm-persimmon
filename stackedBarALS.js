62902fa7b22037e89179044d5a25165a4943d4dd
jghuynh <jghuynh@dons.usfca.edu>
2020-03-14 18:30:42 -0700

    Where are my axes and bars? And why do I have a yellow rect?

--- stackedBarALS.js
+++ stackedBarALS.js
@@ -1,12 +1,19 @@
-var width = 960;
-var height = 500;
 
-var barData = d3.csvParse(await FileAttachment("J_ALS_D3.csv").text(), (d, i, columns) => (d3.autoType(d), d.total = d3.sum(columns, c => d[c]), d)).sort((a, b) => b.total - a.total)
-console.log("Bar data", barData);
 
-d3.csv("J_ALS_D3.csv").then(d => stackBar(d));
+// var barData = d3.csvParse(await FileAttachment("J_ALS_D3.csv").text(), (d, i, columns) => (d3.autoType(d), d.total = d3.sum(columns, c => d[c]), d)).sort((a, b) => b.total - a.total)
+// console.log("Bar data", barData);
 
+barSeries = null;
+
+var barData = d3.csv("J_ALS_D3.csv").then(d => stackBar(d));
+// barSeries = d3.stack()
+//     .keys(barData.columns.slice(0))
+//   (data)
+//     .map(d => (d.forEach(v => v.key = d.key), d));
+// console.log("barSeries", barSeries);
 function stackBar(csv) {
+  var width = 960;
+  var height = 500;
   var myKeys = csv.columns.slice(1);
   // var myKeys = ["WithALSUnit", "WithoutALSUnit"];
   // is there a better way to grab myKeys?
@@ -27,7 +34,6 @@ function stackBar(csv) {
 
   var y = d3.scaleLinear()
 	.rangeRound([height - margin.bottom, margin.top]);
-
   // xAxis
   var xAxis = svg.append("g")
 		.attr("transform", translate(0, height - margin.bottom))
@@ -45,6 +51,33 @@ function stackBar(csv) {
   // so red = true, yellow = false
 
   y.domain([0, d3.max(csv, d => d3.sum(myKeys, k => +d[k]))]).nice();
+
+  // x domain is the Neighborhoods column
+  x.domain(csv.map(d => d.Neighberhoods));
+
+  var group = svg.selectAll("g.layer")
+			.data(d3.stack().keys(myKeys)(csv), d => d.key);
+
+  group.exit().remove()
+
+	group.enter().append("g")
+		.classed("layer", true)
+		.attr("fill", d => colors(d.key));
+
+    // draw bars
+  var bars = svg.selectAll("g.layer").selectAll("rect")
+			.data(d => d, e => e.Neighborhoods);
+
+  bars.exit().remove();
+
+	bars.enter().append("rect")
+		.attr("width", x.bandwidth())
+		.merge(bars)
+	//.transition().duration(speed)
+		.attr("x", d => x(d.Neighborhoods))
+		.attr("y", d => y(d[1]))
+		.attr("height", d => y(d[0]) - y(d[1]));
+
 }
 
 /*
